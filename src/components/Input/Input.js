2256739c192aad5c709dea86bb6e9db0fe1db5a8
saurav0705 <sauravaggarwal98@gmail.com>
2020-03-26 18:36:59 +0530

    completed

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 import './Input.css';
 const Input = ({message,setMessage,sendMessage}) => {
+    const sendMsg = (event) =>{
+        var code = event.keyCode || event.which;
+    if(code === 13) { //13 is the enter keycode
+      event.target.blur();
+      sendMessage(event);
+    }
+}
     return (<>
         <div className="sendbox">
-        <input className="input" type="text" placeholder="type message" value={message} onChange={(event => setMessage(event.target.value))}/>
+        <input className="input" type="text" placeholder="type message" value={message} onKeyPress={(event) =>sendMsg(event)} onChange={(event => setMessage(event.target.value))}/>
         <button  className="send" onClick={(event)=>sendMessage(event)}>send</button>
         </div>
         </>
