0fb0652c502c90ab451b374825261cdc4c19fe3e
saurav0705 <sauravaggarwal98@gmail.com>
2020-03-26 18:04:15 +0530

    completed chat room

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -14,7 +14,7 @@ const Chat = ({location}) => {
     const [users, setUsers] = useState('');
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
-    const ENDPOINT = "localhost:5000"
+    const ENDPOINT = "https://chatroom98.herokuapp.com/"
 
 
     useEffect(() => {
